e3b4e4229a7c3cc54e62b923a1f554e88666dd7a
Nupur <nupurk15@gmail.com>
2016-02-02 04:00:57 -0500

    adding nav routes

--- app/router.js
+++ app/router.js
@@ -5,7 +5,10 @@ const Router = Ember.Router.extend({
   location: config.locationType
 });
 
+// Setting router for navigation menu. Home page is the default index page nad doesn't need one specifically.
 Router.map(function() {
+	this.route('about', { path: '/about' });
+  	this.route('contact', { path: '/contact' });
 });
 
 export default Router;
